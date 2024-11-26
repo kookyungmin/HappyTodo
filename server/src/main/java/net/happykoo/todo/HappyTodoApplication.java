f133758fda7f336e84383cad478bbc34f073e15e
kookyungmin <rudals4549@naver.com>
2024-11-26 22:07:15 +0900

    feat: server project

---
+++ server/src/main/java/net/happykoo/todo/HappyTodoApplication.java
@@ -0,0 +1,11 @@
+package net.happykoo.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTodoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HappyTodoApplication.class, args);
+    }
+}
---
+++ server/src/main/java/net/happykoo/todo/core/datasource/DataSourceConfig.java
@@ -0,0 +1,60 @@
+package net.happykoo.todo.core.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource dataSource() {
+        HikariDataSource dataSource = DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+
+        return dataSource;
+    }
+
+    @Bean("sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setConfiguration(getMybatisConfig());
+        sqlSessionFactoryBean.setTypeAliasesPackage("net.happykoo.todo.domain.**.dto");
+
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resource = resolver.getResources("mybatis/**/*.xml");
+        sqlSessionFactoryBean.setMapperLocations(resource);
+
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSession sqlSession(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    private org.apache.ibatis.session.Configuration getMybatisConfig() {
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setUseColumnLabel(true);
+        configuration.setDefaultStatementTimeout(10000);
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setCallSettersOnNulls(true);
+        configuration.setLogPrefix("[SQL]");
+
+        return configuration;
+    }
+}
