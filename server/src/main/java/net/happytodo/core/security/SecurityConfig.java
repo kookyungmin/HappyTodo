bb78c0cf44a258e1ee0618e9b36946760c7cc8c1
kookyungmin <rudals4549@naver.com>
2024-12-22 22:02:51 +0900

    feat: cors 로직 추가

--- server/src/main/java/net/happytodo/core/security/SecurityConfig.java
+++ server/src/main/java/net/happytodo/core/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package net.happytodo.core.security;
 import net.happytodo.core.exception.CustomExceptionHandler;
 import net.happytodo.core.security.authentication.CustomAuthenticationProvider;
 import net.happytodo.core.security.filter.CustomUsernamePasswordAuthenticationFilter;
+import org.springframework.cache.interceptor.CacheableOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,6 +17,10 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static net.happytodo.core.exception.CustomExceptionCode.*;
 
@@ -34,6 +39,7 @@ public class SecurityConfig {
             .formLogin(formLogin -> formLogin.disable())
             .httpBasic(httpBasic -> httpBasic.disable())
             .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(configurationSource()))
             .addFilterBefore(new CustomUsernamePasswordAuthenticationFilter(authenticationManager, securityContextRepository),
                     UsernamePasswordAuthenticationFilter.class)
             .exceptionHandling(ex -> ex.accessDeniedHandler(getAccessDeniedHandler())
@@ -66,6 +72,19 @@ public class SecurityConfig {
         //인증 O 인가 X (401 Authorization)
         return (request, response, accessDeniedException) -> {
             CustomExceptionHandler.writeSecurityExceptionResponse(response, USER_UNAUTHORIZED);
+        };
+    }
+
+    private CorsConfigurationSource configurationSource() {
+        return request -> {
+            CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+            corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+            corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+            corsConfiguration.setAllowCredentials(true);
+            corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+
+            return corsConfiguration;
         };
     }
 
